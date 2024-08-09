a48053d2e7be7c10a500b597bb01626543d33791
Pimlapas Narongrit <56467510+LapasN@users.noreply.github.com>
2024-08-09 15:28:08 -0400

    app.py

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ def calculate_covered_call_payoff_bs(asset_prices, purchase_price, strike_price,
     call_option_prices = np.array([black_scholes_call(S, strike_price, T, r, sigma) for S in asset_prices])
     long_asset_payoff = call_option_prices - strike_price
     short_call_payoff = np.where(call_option_prices > strike_price, strike_price - call_option_prices + premium, premium)
-    payoffs = long_asset_payoff + short_call_payoff
+    payoffs = (long_asset_payoff + short_call_payoff)*2
     
     return payoffs
 
