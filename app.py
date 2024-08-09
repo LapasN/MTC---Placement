b5580bbb9218cc6503e9620fc505f6fb15cd575e
Pimlapas Narongrit <56467510+LapasN@users.noreply.github.com>
2024-08-09 16:27:32 -0400

    app.py

--- app.py
+++ app.py
@@ -61,8 +61,8 @@ def calculate_straddle_payoff(asset_prices, strike_price, T, r, sigma, premium_c
 
 def calculate_covered_call_payoff_bs(asset_prices, purchase_price, strike_price, T, r, sigma, premium):
     call_option_prices = np.array([black_scholes_call(S, strike_price, T, r, sigma) for S in asset_prices])
-    long_asset_payoff = call_option_prices - purchase_price
-    short_call_payoff = np.where(call_option_prices > strike_price, premium - (asset_prices - strike_price), premium)
+    long_asset_payoff = asset_prices - call_option_prices
+    short_call_payoff = np.where(asset_prices > strike_price, premium - (asset_prices - strike_price), premium)
     payoffs = long_asset_payoff + short_call_payoff
     
     return payoffs
