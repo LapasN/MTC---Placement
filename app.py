8acb912ea0fe8d1b6f496772b7108c6b6203bca3
Pimlapas Narongrit <56467510+LapasN@users.noreply.github.com>
2024-08-14 13:33:23 -0400

    Update app.py

--- app.py
+++ app.py
@@ -211,7 +211,7 @@ strategy = st.selectbox("Select Strategy", ["Call", "Put", "Straddle", "Covered 
 # Strategy parameters
 asset_price = st.number_input('Underlying Asset Price', value=most_recent_close, key=f'asset_price_{selected_symbol}')
 strike_price = st.number_input('Strike Price', value=int(round(asset_price, 0)), step=1, key=f'strike_{strategy}')
-premium = st.number_input('Premium',value=10, step = 0.01, key=f'premium_{strategy}')
+premium = st.number_input('Premium',value=10.0, step = 0.01, key=f'premium_{strategy}')
 current_date = st.date_input('Current Date', key=f'current_{strategy}')
 expiration_date = st.date_input('Expiration Date', key=f'expiry_{strategy}')
 
